083afac8dcaa4ca47e3364caa72bb6143466fe57
wysiwys <144733119+wysiwys@users.noreply.github.com>
2025-01-08 14:25:31 +0100

    formatting

--- third_person_beginner/tutorial_1/src/main.rs
+++ third_person_beginner/tutorial_1/src/main.rs
@@ -18,7 +18,7 @@ fn spawn_camera(mut commands: Commands) {
     let camera = (
         Camera3d::default(),
         Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        );
+    );
 
     commands.spawn(camera);
 }
